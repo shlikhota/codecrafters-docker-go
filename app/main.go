300e9b133af26efb2f43c8cc9e50f6f7fa6ea9f3
Evgenii Shlikhota <shlikhota@gmail.com>
2022-08-09 23:28:14 +0200

    add filesystem isolation

--- app/main.go
+++ app/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
+	"path"
+	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
+
+	chrootDir, _ := ioutil.TempDir("", "")
+	if err := copyFile(command, chrootDir); err != nil {
+		log.Fatal(err)
+	}
+	if err := createDevNullFile(chrootDir); err != nil {
+		log.Fatal(err)
+	}
+
+	syscall.Chroot(chrootDir)
 
 	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
@@ -22,4 +37,33 @@ func main() {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
+}
+
+func copyFile(sourceFilepath, destinationPath string) error {
+	sourceFile, err := os.Open(sourceFilepath)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+	stat, _ := sourceFile.Stat()
+
+	destinationFilepath := path.Join(destinationPath, sourceFilepath)
+	os.MkdirAll(path.Dir(destinationFilepath), 0750)
+	fmt.Printf("Copy file from %s to %s\n", sourceFilepath, destinationFilepath)
+	destinationFile, err := os.OpenFile(destinationFilepath, os.O_RDWR|os.O_CREATE, stat.Mode())
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	return err
+}
+
+func createDevNullFile(destinationDir string) error {
+	if err := os.MkdirAll(path.Join(destinationDir, "dev"), 0750); err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(path.Join(destinationDir, "dev", "null"), []byte{}, 0644)
 }
