f3fd087f851a995dc5d4dace4effa72206faf9fb
Evgenii Shlikhota <shlikhota@gmail.com>
2022-08-12 23:04:29 +0200

    add default tag

--- app/docker_registry.go
+++ app/docker_registry.go
@@ -1,1 +1,143 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+)
+
+type image struct {
+	repository string
+	reference  string
+	manifest   imageManifestResponse
+}
+
+type Docker struct {
+	authToken string
+	image     image
+}
+
+type authResponse struct {
+	Token string `json:"token"`
+}
+
+type imageManifestResponse struct {
+	Name     string              `json:"name"`
+	Tag      string              `json:"tag"`
+	FsLayers []map[string]string `json:"fsLayers"`
+}
+
+const baseDockerHubUrl = "registry.hub.docker.com"
+const dockerAuthUrl string = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:%s:pull"
+
+func CreateDocker(dockerImage string) (*Docker, error) {
+	imageName := dockerImage
+	tag := "latest"
+	if strings.Index(dockerImage, ":") != -1 {
+		imageName = dockerImage[0:strings.Index(dockerImage, ":")]
+		tag = dockerImage[strings.Index(dockerImage, ":")+1:]
+	}
+	imageName = fmt.Sprintf("library/%s", imageName)
+	docker := &Docker{image: image{repository: imageName, reference: tag}}
+	if err := docker.auth(); err != nil {
+		return nil, err
+	}
+
+	if err := docker.fetchImageManifest(); err != nil {
+		return nil, err
+	}
+
+	return docker, nil
+}
+
+func (d *Docker) auth() error {
+	url := fmt.Sprintf(dockerAuthUrl, d.image.repository)
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("auth status code: %d\n", response.StatusCode)
+	}
+
+	respBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	authResponse := &authResponse{}
+	if err := json.Unmarshal(respBody, authResponse); err != nil {
+		return err
+	}
+
+	d.authToken = authResponse.Token
+	return nil
+}
+
+func (d *Docker) fetchImageManifest() error {
+	url := fmt.Sprintf("https://%s/v2/%s/manifests/%s", baseDockerHubUrl, d.image.repository, d.image.reference)
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+	req.Header.Set("Docker-Distribution-API-Version", "registry/2.0")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", d.authToken))
+
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	respBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("fetch image manifest status code: %d\n", response.StatusCode)
+	}
+
+	manifestResponse := imageManifestResponse{}
+	if err := json.Unmarshal(respBody, &manifestResponse); err != nil {
+		return err
+	}
+
+	d.image.manifest = manifestResponse
+
+	return nil
+}
+
+func (d *Docker) Pull(destination string) (*[]string, error) {
+	var files []string
+	for _, layer := range d.image.manifest.FsLayers {
+		url := fmt.Sprintf("https://%s/v2/%s/blobs/%s", baseDockerHubUrl, d.image.manifest.Name, layer["blobSum"])
+		req, _ := http.NewRequest(http.MethodGet, url, nil)
+		req.Header.Set("Docker-Distribution-API-Version", "registry/2.0")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", d.authToken))
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return nil, err
+		}
+
+		layerFilename := path.Join(destination, layer["blobSum"]+".tar.gz")
+		layerFile, err := os.OpenFile(layerFilename, os.O_RDWR|os.O_CREATE, 0750)
+		if err != nil {
+			return nil, err
+		}
+		defer layerFile.Close()
+
+		_, err = io.Copy(layerFile, resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		files = append(files, path.Join(destination, layerFile.Name()))
+	}
+	return &files, nil
+}
--- app/main.go
+++ app/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"path"
-	"strings"
 	"syscall"
 )
 
@@ -46,7 +42,7 @@ func main() {
 	cmd.Stderr = os.Stderr
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWPID,
+		Cloneflags: syscall.CLONE_NEWPID, // linux only
 	}
 
 	err := cmd.Run()
@@ -88,131 +84,4 @@ func createDevNullFile(destinationDir string) error {
 func extractTar(archiveFilename, destination string) error {
 	cmd := exec.Command("tar", []string{"xf", archiveFilename, "-C", destination}...)
 	return cmd.Run()
-}
-
-type image struct {
-	repository string
-	reference  string
-	manifest   imageManifestResponse
-}
-
-type Docker struct {
-	authToken string
-	image     image
-}
-
-type authResponse struct {
-	Token string `json:"token"`
-}
-
-type imageManifestResponse struct {
-	Name     string              `json:"name"`
-	Tag      string              `json:"tag"`
-	FsLayers []map[string]string `json:"fsLayers"`
-}
-
-const baseDockerHubUrl = "registry.hub.docker.com"
-const dockerAuthUrl string = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:%s:pull"
-
-func CreateDocker(dockerImage string) (*Docker, error) {
-	imageName := dockerImage[0:strings.Index(dockerImage, ":")]
-	tag := dockerImage[strings.Index(dockerImage, ":")+1:]
-	imageName = fmt.Sprintf("library/%s", imageName)
-	docker := &Docker{image: image{repository: imageName, reference: tag}}
-	if err := docker.auth(); err != nil {
-		return nil, err
-	}
-
-	if err := docker.fetchImageManifest(); err != nil {
-		return nil, err
-	}
-
-	return docker, nil
-}
-
-func (d *Docker) auth() error {
-	url := fmt.Sprintf(dockerAuthUrl, d.image.repository)
-	response, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("auth status code: %d\n", response.StatusCode)
-	}
-
-	respBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return err
-	}
-
-	authResponse := &authResponse{}
-	if err := json.Unmarshal(respBody, authResponse); err != nil {
-		return err
-	}
-
-	d.authToken = authResponse.Token
-	return nil
-}
-
-func (d *Docker) fetchImageManifest() error {
-	url := fmt.Sprintf("https://%s/v2/%s/manifests/%s", baseDockerHubUrl, d.image.repository, d.image.reference)
-	req, _ := http.NewRequest(http.MethodGet, url, nil)
-	req.Header.Set("Docker-Distribution-API-Version", "registry/2.0")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", d.authToken))
-
-	response, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-
-	respBody, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return err
-	}
-
-	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("fetch image manifest status code: %d\n", response.StatusCode)
-	}
-
-	manifestResponse := imageManifestResponse{}
-	if err := json.Unmarshal(respBody, &manifestResponse); err != nil {
-		return err
-	}
-
-	d.image.manifest = manifestResponse
-
-	return nil
-}
-
-func (d *Docker) Pull(destination string) (*[]string, error) {
-	var files []string
-	for _, layer := range d.image.manifest.FsLayers {
-		url := fmt.Sprintf("https://%s/v2/%s/blobs/%s", baseDockerHubUrl, d.image.manifest.Name, layer["blobSum"])
-		req, _ := http.NewRequest(http.MethodGet, url, nil)
-		req.Header.Set("Docker-Distribution-API-Version", "registry/2.0")
-		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", d.authToken))
-
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			return nil, err
-		}
-
-		layerFilename := path.Join(destination, layer["blobSum"]+".tar.gz")
-		layerFile, err := os.OpenFile(layerFilename, os.O_RDWR|os.O_CREATE, 0750)
-		if err != nil {
-			return nil, err
-		}
-		defer layerFile.Close()
-
-		_, err = io.Copy(layerFile, resp.Body)
-		if err != nil {
-			return nil, err
-		}
-
-		files = append(files, path.Join(destination, layerFile.Name()))
-	}
-	return &files, nil
 }
